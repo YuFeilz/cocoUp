48d9e933d225691decd4d40a56d71269ed6fdace
muziliu <muziliu0124@163.com>
2018-05-19 23:14:41 +0800

    -

--- route/admin.js
+++ route/admin.js
@@ -50,7 +50,55 @@ module.exports = function() {
         res.render('./admin/index.ejs', {});
     })
     router.get('/banner', (req, res) => {
-        res.render('./admin/banner.ejs', {});
+        switch(req.query.act){
+            // 修改
+            case 'mod':
+                db.query(`SELECT * FROM　banner_table`,(err,data)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                })
+            break;
+            // 删除
+            case 'del':
+                db.query(`DELETE FROM banner_table WHERE ID=${req.query.id}`,(err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(500).send('数据库错误').end();
+                    }else{
+                        res.redirect('/admin/banner');
+                    }
+                })
+            break;
+            // 正常显示数据
+            default:
+                db.query(`SELECT * FROM banner_table`,(err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(500).send('数据库错误').end();
+                    }else{
+                        res.render('./admin/banner.ejs', {banners:data});
+                    }
+                })
+            break;
+        } 
+    })
+    router.post('/banner',(req,res)=>{
+        var title=req.body.title;
+        var description=req.body.description;
+        var href=req.body.href;
+        if(!title||!description||!href){
+            res.status(400).send('数据异常').end();
+        }else{
+            db.query(`INSERT INTO banner_table(title,description,href) VALUE('${title}','${description}','${href}')`,(err,data)=>{
+                if(err){
+                    console.log(err);
+                    res.status(500).send('数据库错误').end();
+                }else{
+                    res.redirect('/admin/banner');
+                }
+            })
+        }
     })
     return router;
 }
